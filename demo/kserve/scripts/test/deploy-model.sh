868ec1e7161248edaa2d4628b0554b2b423a81d1
heyselbi <selbi@redhat.com>
2024-03-12 12:42:38 -0400

    save ovms files
    Signed-off-by: heyselbi <selbi@redhat.com>

--- demo/kserve/scripts/test/deploy-model.sh
+++ demo/kserve/scripts/test/deploy-model.sh
@@ -36,33 +36,33 @@ ACCESS_KEY_ID=THEACCESSKEY
 oc get ns ${MINIO_NS}
 if [[ $? ==  1 ]]
 then
-  oc new-project ${MINIO_NS}
-  SECRET_ACCESS_KEY=$(openssl rand -hex 32)
-  sed "s/<accesskey>/$ACCESS_KEY_ID/g"  ./custom-manifests/minio/minio.yaml | sed "s+<secretkey>+$SECRET_ACCESS_KEY+g" | tee ${BASE_DIR}/minio-current.yaml | oc -n ${MINIO_NS} apply -f -
-  sed "s/<accesskey>/$ACCESS_KEY_ID/g" ./custom-manifests/minio/minio-secret.yaml | sed "s+<secretkey>+$SECRET_ACCESS_KEY+g" |sed "s/<minio_ns>/$MINIO_NS/g" | tee ${BASE_DIR}/minio-secret-current.yaml | oc -n ${MINIO_NS} apply -f - 
+oc new-project ${MINIO_NS}
+SECRET_ACCESS_KEY=$(openssl rand -hex 32)
+sed "s/<accesskey>/$ACCESS_KEY_ID/g"  ./custom-manifests/minio/minio.yaml | sed "s+<secretkey>+$SECRET_ACCESS_KEY+g" | tee ${BASE_DIR}/minio-current.yaml | oc -n ${MINIO_NS} apply -f -
+sed "s/<accesskey>/$ACCESS_KEY_ID/g" ./custom-manifests/minio/minio-secret.yaml | sed "s+<secretkey>+$SECRET_ACCESS_KEY+g" |sed "s/<minio_ns>/$MINIO_NS/g" | tee ${BASE_DIR}/minio-secret-current.yaml | oc -n ${MINIO_NS} apply -f - 
 else
-  SECRET_ACCESS_KEY=$(oc get pod minio  -n minio -ojsonpath='{.spec.containers[0].env[1].value}')
-  sed "s/<accesskey>/$ACCESS_KEY_ID/g"  ./custom-manifests/minio/minio.yaml | sed "s+<secretkey>+$SECRET_ACCESS_KEY+g" | tee ${BASE_DIR}/minio-current.yaml 
-  sed "s/<accesskey>/$ACCESS_KEY_ID/g" ./custom-manifests/minio/minio-secret.yaml | sed "s+<secretkey>+$SECRET_ACCESS_KEY+g" |sed "s/<minio_ns>/$MINIO_NS/g" | tee ${BASE_DIR}/minio-secret-current.yaml 
+SECRET_ACCESS_KEY=$(oc get pod minio  -n minio -ojsonpath='{.spec.containers[0].env[1].value}')
+sed "s/<accesskey>/$ACCESS_KEY_ID/g"  ./custom-manifests/minio/minio.yaml | sed "s+<secretkey>+$SECRET_ACCESS_KEY+g" | tee ${BASE_DIR}/minio-current.yaml 
+sed "s/<accesskey>/$ACCESS_KEY_ID/g" ./custom-manifests/minio/minio-secret.yaml | sed "s+<secretkey>+$SECRET_ACCESS_KEY+g" |sed "s/<minio_ns>/$MINIO_NS/g" | tee ${BASE_DIR}/minio-secret-current.yaml 
 fi
 sed "s/<minio_ns>/$MINIO_NS/g" ./custom-manifests/minio/serviceaccount-minio.yaml | tee ${BASE_DIR}/serviceaccount-minio-current.yaml 
 
 if ! oc get ns ${TEST_NS}
 then
-    oc new-project ${TEST_NS}
+  oc new-project ${TEST_NS}
 else
-  echo "* ${TEST_NS} already exists."
+echo "* ${TEST_NS} already exists."
 fi
 
-ISVC_NAME=caikit-tgis-isvc"${INF_PROTO}"
+# ISVC_NAME=caikit-tgis-isvc"${INF_PROTO}"
 
-if ! oc get isvc ${ISVC_NAME} --no-headers >/dev/null; then
-  echo "* The ISVC ${ISVC_NAME} already exists. Please remove it."
-  exit 1
-fi
+# if oc get isvc ${ISVC_NAME} --no-headers >/dev/null; then
+#   echo "* The ISVC ${ISVC_NAME} already exists. Please remove it."
+#   exit 1
+# fi
 
-echo "Creating ISVC ${ISVC_NAME}"
-oc apply -f ./custom-manifests/caikit/caikit-tgis/caikit-tgis-servingruntime"${INF_PROTO}".yaml -n ${TEST_NS}
+# echo "Creating ISVC ${ISVC_NAME}"
+# oc apply -f ./custom-manifests/caikit/caikit-tgis/caikit-tgis-servingruntime"${INF_PROTO}".yaml -n ${TEST_NS}
 
 oc apply -f ${BASE_DIR}/minio-secret-current.yaml -n ${TEST_NS}
 oc apply -f ${BASE_DIR}/serviceaccount-minio-current.yaml -n ${TEST_NS}
